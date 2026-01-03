f405d21d5eb4551effbe9d5e74016f9d95b4fce2
Thomas Askharoun <151990913+askharoun@users.noreply.github.com>
2026-01-02 22:26:29 -0500

    Delete PWAs/Deacon directory

--- PWAs/Deacon/js/app.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var App = require('./components/OPApp.react');
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('OP_app')
-);
